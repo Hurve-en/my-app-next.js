28fea1414576113cb125574f4ff0e3580b0c2c32
Hurve-en <velosohurveenrayford@gmail.com>
2026-09-09 16:08:26 +0800

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
 import "./globals.css";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import "./page";
 
-const plusJakarta = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
-
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={plusJakarta.className}>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
