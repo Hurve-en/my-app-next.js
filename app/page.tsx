b2ffb6b28907b8dd8dcc8432a80bc061ea07f928
Hurve-en <velosohurveenrayford@gmail.com>
2026-09-11 11:23:16 +0800

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
+import Image from "next/image";
 export default function Page() {
-  return <h1 className="text">Hello Next.js</h1>;
+  return (
+    <>
+      <header className="nav">
+        <p>Available for New Project</p>
+        <div className="links">
+          <a href="/work">Work</a>
+          <a href="/service">Service</a>
+          <a href="/experince">Experience</a>
+          <a href="/contact">Contact</a>
+        </div>
+        <button className="signIn">Lets Talk</button>
+      </header>
+
+      <main>
+        <h1>Hurve-en</h1>
+
+        <h2>Full-Stack Developer</h2>
+
+        <p>Developing meaningful apps that help people on a daily basis.</p>
+
+        <Image
+          className="heroPic"
+          src="/profiles.jpeg"
+          alt="pfp"
+          width={340}
+          height={420}
+          priority
+        />
+
+        <button>Github</button>
+        <button>Instagram</button>
+        <button>LinkedIn</button>
+      </main>
+    </>
+  );
 }
